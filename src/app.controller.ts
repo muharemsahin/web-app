402e861ab5ca1938d0abb1dd9ec181e62819f0c6
Muharem Sahinovic <muharem.sahinovic@live.com>
2021-10-13 12:29:51 +0200

    Create the administrator tablse service

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,20 @@
 import { Controller, Get } from '@nestjs/common';
+import { Administrator } from 'entities/administrator.entity';
 import { AppService } from './app.service';
+import { AdministratorService } from './services/administrator/administrator.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private administratorService: AdministratorService) {}
+  
 
   @Get()
   getHello(): string {
-    return this.appService.getHello();
+    return "Home page'";
+  }
+
+  @Get('api/administrator')
+  getAllAdmins(): Promise<Administrator[]>{
+    return this.administratorService.getAll();
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,27 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DatabaseConfiguration } from 'config/databaseconfiguration';
+import { Administrator } from 'entities/administrator.entity';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AdministratorService } from './services/administrator/administrator.service';
+
+
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: DatabaseConfiguration.hostname,
+      port: 3306,
+      username: DatabaseConfiguration.username,
+      password: DatabaseConfiguration.password,
+      database: DatabaseConfiguration.database,
+      entities: [Administrator]
+    }),
+    TypeOrmModule.forFeature([Administrator])
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, AdministratorService],
 })
 export class AppModule {}
